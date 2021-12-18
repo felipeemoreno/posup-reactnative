47cfeb164d36b97e9d86107b1a13eada3aafa8ec
Felipe Estevão Moreno <contato@wr5solutions.com.br>
2021-12-17 23:32:23 -0300

    build: navigation ok

---
+++ Pages/ProductsPage.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { SafeAreaView, VirtualizedList, StyleSheet, Text, View, Image, TouchableOpacity, } from 'react-native';
+
+import illustracaoHome from '../assets/Illustracao_home.png';
+
+const listaProdutos = [
+  {
+    id: 1,
+    name: "Miojo com Queijo Ralado",
+    price: 15.99,
+    image: null
+  },
+  {
+    id: 2,
+    name: "Strogonoff de Camarão",
+    price: 55.99,
+    image: null
+  },
+  {
+    id: 3,
+    name: "Lasanha Quatro Queijos",
+    price: 45.99,
+    image: null
+  }
+]
+
+const getItem = (listaProdutos, index) => (listaProdutos[index]);
+
+const getItemCount = (listaProdutos) => listaProdutos.length;
+
+const Item = ({ product, navigationObj }) => (
+  <View style={styles.item}>
+    <TouchableOpacity  onPress={() => navigationObj.navigate("ProductDetails", { produtoObj: product })}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{marginRight: 10}}>
+          <Image source={illustracaoHome} style={{width: 40, height: 40}} />
+        </View>
+        <View>
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.title}>{product.price}</Text>
+        </View>
+      </View>
+      
+    </TouchableOpacity>
+  </View>
+);
+
+const ProductsPage =  ({ navigation }) => {
+  return (
+    <SafeAreaView  style={styles.container}>
+      <View style={{flex: 1, justifyContent: "center", paddingTop: 10}}>
+        <Text style={styles.homeTitle}>PRODUTOS</Text>
+      </View>
+      <View style={{flex: 4, width: '100%', paddingHorizontal: 10}}>
+        <VirtualizedList
+          data={listaProdutos}
+          // initialNumToRender={2} opcional qual item começa
+          renderItem={({ item }) => <Item product={item} navigationObj={navigation} />}
+          keyExtractor={item => item.id}
+          getItemCount={getItemCount}
+          getItem={getItem}
+          />
+        </View>
+    </SafeAreaView >
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  homeTitle: {
+    fontSize: 50,  
+    fontFamily: 'Poppins_700Bold',
+    color: "#FF4B3A",
+    borderBottomColor: '#FF4B3A50',
+    borderBottomWidth: 1,
+  },
+  homeIllustracao: {
+    width: '100%',
+    height: '100%'
+  },
+  btnInicio: {
+    color: "#FF4B3A",
+    backgroundColor: "#FFF",
+    paddingHorizontal: 60,
+    paddingVertical: 20,
+    borderRadius: 100
+  },
+  item: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "#FF4B3A22",
+    padding: 20,
+    marginBottom: 5
+  }
+});
+
+export default ProductsPage;
--- Pages/homePage.js
+++ Pages/homePage.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
+
 import illustracaoHome from '../assets/Illustracao_home.png';
 
-function HomePage() {
+const HomePage = ({ navigation }) => {
   return (
-    <View style={[styles.container, colors.bkgOrange]}>
+    <View style={styles.container}>
       <View style={{flex: 1, justifyContent: "center", paddingTop: 10}}>
-        <Text style={[styles.homeTitle, colors.textWhite]}>RANGO{'\n'}EXPRESS</Text>
+        <Text style={styles.homeTitle}>RANGO{'\n'}EXPRESS</Text>
       </View>
       <View style={{flex: 2, width: '100%', alignItems: "center"}}>
         <Image source={illustracaoHome} style={{width: "55%", height: "100%"}} />
       </View>
       <View style={{flex: 1, justifyContent: "center"}}>
-        <TouchableOpacity style={styles.btnInicio}>
+        <TouchableOpacity 
+          style={styles.btnInicio}
+          onPress={() => navigation.navigate('Products')}>
           <Text>Ver o Cardápio</Text>
         </TouchableOpacity>
       </View>
@@ -21,25 +24,19 @@ function HomePage() {
   )
 }
 
-const colors = StyleSheet.create({
-  bkgOrange: {
-    backgroundColor: '#FF4B3A',
-  },
-  textWhite: {
-    color: "#FFF"
-  }
-})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#FF4B3A',
   },
   homeTitle: {
     fontSize: 50,  
     fontFamily: 'Poppins_700Bold',
-    lineHeight: 52
+    lineHeight: 54,
+    color: "#FFF"
   },
   homeIllustracao: {
     width: '100%',
@@ -51,7 +48,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 60,
     paddingVertical: 20,
     borderRadius: 100
-    
   }
 });
 export default HomePage
